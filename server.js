1b475fcd324f8ce831e92faadb38fd75c9867b85
tokaandesigns-a11y <tokaandesigns@gmail.com>
2026-03-17 23:24:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ app.post('/render-svg', (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT;
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log('renderer running on port ' + PORT);
 });
